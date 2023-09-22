2add4237a3d8af2b237331727679b84a3397af85
Dinethri Gunawardhana <dinethriupeksha200@gmail.com>
2023-09-22 23:42:11 +0530

    Route Details Add

---
+++ nextbus_driver/lib/components/textFieldDropDown.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DropDownTextField extends StatelessWidget {
+  const DropDownTextField({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- nextbus_driver/lib/pages/routeDetailsAddPage.dart
+++ nextbus_driver/lib/pages/routeDetailsAddPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:nextbus_driver/colors.dart';
+import 'package:nextbus_driver/methods/sizes.dart';
 
 class RouteDetailsAdd extends StatefulWidget {
   const RouteDetailsAdd({Key? key}) : super(key: key);
@@ -25,6 +26,78 @@ class _RouteDetailsAddState extends State<RouteDetailsAdd> {
                 child: Image.asset(
                   'images/VerLogoBlue 1.png',
                 )),
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: Container(
+                width: getPageWidth(context),
+                height: 480,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(
+                    color: Colors.white, //  border color
+                    width: 2.0,          //  the border width
+                  ),
+                  borderRadius: BorderRadius.circular(20), // border radius
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.grey, // shadow color
+                      offset: Offset(0, 3), // shadow's offset
+                      blurRadius: 5,       // blur radius
+                    ),
+                  ],
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Text('Select Route Number : ',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold
+                          ),),
+                          SizedBox(width: 5,),
+                          Text('*',
+                          style: TextStyle(color: Colors.red,)),
+                        ],
+                      ),
+                      SizedBox(height: 20,),
+
+                      Row(
+                        children: [
+                          Text('Enter Bus Number : ',
+                            style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold
+                            ),),
+                          SizedBox(width: 5,),
+                          Text('*',
+                              style: TextStyle(color: Colors.red,)),
+                        ],
+                      ),
+                      SizedBox(height: 20,),
+
+                      Row(
+                        children: [
+                          Text('Number of Seats in Bus: ',
+                            style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold
+                            ),),
+                          SizedBox(width: 5,),
+                          Text('*',
+                              style: TextStyle(color: Colors.red,)),
+                        ],
+                      ),
+
+                    ],
+                  ),
+                ),
+              ),
+            )
+
           ],
 
         ),
