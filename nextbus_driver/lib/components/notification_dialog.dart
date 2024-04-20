347b34658ce044777e32fa73cdd9db218eb95a7f
Shahiru Edirisinghe <98458540+funky-dev99@users.noreply.github.com>
2024-04-21 00:16:26 +0530

    Update notification_dialog.dart

--- nextbus_driver/lib/components/notification_dialog.dart
+++ nextbus_driver/lib/components/notification_dialog.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
@@ -11,39 +10,35 @@ import '../models/trip_details.dart';
 import '../pages/new_trip_page.dart';
 import 'loading.dart';
 
-
-class NotificationDialog extends StatefulWidget
-{
+class NotificationDialog extends StatefulWidget {
   TripDetails? tripDetailsInfo;
 
-  NotificationDialog({super.key, this.tripDetailsInfo,});
+  NotificationDialog({
+    super.key,
+    this.tripDetailsInfo,
+  });
 
   @override
   State<NotificationDialog> createState() => _NotificationDialogState();
 }
 
-class _NotificationDialogState extends State<NotificationDialog>
-{
+class _NotificationDialogState extends State<NotificationDialog> {
   String tripRequestStatus = "";
   CommonMethods cMethods = CommonMethods();
+  String driversRouteNumber = "";
 
-
-  cancelNotificationDialogAfter20Sec()
-  {
+  cancelNotificationDialogAfter20Sec() {
     const oneTickPerSecond = Duration(seconds: 1);
 
-    var timerCountDown = Timer.periodic(oneTickPerSecond, (timer)
-    {
+    var timerCountDown = Timer.periodic(oneTickPerSecond, (timer) {
       driverTripRequestTimeout = driverTripRequestTimeout - 1;
 
-      if(tripRequestStatus == "accepted")
-      {
+      if (tripRequestStatus == "accepted") {
         timer.cancel();
         driverTripRequestTimeout = 20;
       }
 
-      if(driverTripRequestTimeout == 0)
-      {
+      if (driverTripRequestTimeout == 0) {
         Navigator.pop(context);
         timer.cancel();
         driverTripRequestTimeout = 20;
@@ -52,74 +47,85 @@ class _NotificationDialogState extends State<NotificationDialog>
     });
   }
 
+  Future<void> getDriversRoute() async {
+    DatabaseReference driverRef = FirebaseDatabase.instance
+        .ref()
+        .child("drivers")
+        .child(FirebaseAuth.instance.currentUser!.uid);
+
+    // Get the route number from Firebase
+    DataSnapshot snapshot = await driverRef.child("routeNumber").get();
+    if (snapshot.exists) {
+      // Update the state if the value is fetched successfully
+      setState(() {
+        driversRouteNumber = snapshot.value.toString();
+        print(
+            "Driver's Route Number: $driversRouteNumber"); // Print the route number in the terminal
+      });
+    } else {
+      print("Route number not found.");
+    }
+  }
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-
+    getDriversRoute();
     cancelNotificationDialogAfter20Sec();
   }
 
-  checkAvailabilityOfTripRequest(BuildContext context) async
-  {
+  checkAvailabilityOfTripRequest(BuildContext context) async {
     showDialog(
       barrierDismissible: false,
       context: context,
-      builder: (BuildContext context) => LoadingDialog(messageText: 'please wait...',),
+      builder: (BuildContext context) => LoadingDialog(
+        messageText: 'please wait...',
+      ),
     );
 
-    DatabaseReference driverTripStatusRef = FirebaseDatabase.instance.ref()
+    DatabaseReference driverTripStatusRef = FirebaseDatabase.instance
+        .ref()
         .child("drivers")
         .child(FirebaseAuth.instance.currentUser!.uid)
         .child("newTripStatus");
 
-    await driverTripStatusRef.once()
-        .then((snap)
-    {
+    await driverTripStatusRef.once().then((snap) {
       Navigator.pop(context);
       Navigator.pop(context);
 
       String newTripStatusValue = "";
-      if(snap.snapshot.value != null)
-      {
+      if (snap.snapshot.value != null) {
         newTripStatusValue = snap.snapshot.value.toString();
-      }
-      else
-      {
-
+      } else {
         snackBar(context, "Trip Request Not Found.", Colors.white);
       }
 
-      if(newTripStatusValue == widget.tripDetailsInfo!.tripID)
-      {
+      if (newTripStatusValue == widget.tripDetailsInfo!.tripID) {
         driverTripStatusRef.set("accepted");
 
         //disable homepage location updates
         // cMethods.turnOffLocationUpdatesForHomePage();
         ///edited as we want for testing
 
-        Navigator.push(context, MaterialPageRoute(builder: (c)=> NewTripPage(newTripDetailsInfo: widget.tripDetailsInfo)));
-      }
-      else if(newTripStatusValue == "cancelled")
-      {
-        snackBar(context, "Trip Request has been Cancelled by user.", Colors.white);
-      }
-      else if(newTripStatusValue == "timeout")
-      {
-
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (c) =>
+                    NewTripPage(newTripDetailsInfo: widget.tripDetailsInfo)));
+      } else if (newTripStatusValue == "cancelled") {
+        snackBar(
+            context, "Trip Request has been Cancelled by user.", Colors.white);
+      } else if (newTripStatusValue == "timeout") {
         snackBar(context, "Trip Request timed out.", Colors.white);
-      }
-      else
-      {
-
+      } else {
         snackBar(context, "Trip Request removed. Not Found.", Colors.white);
       }
     });
   }
 
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
     return Dialog(
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(12),
@@ -135,18 +141,21 @@ class _NotificationDialogState extends State<NotificationDialog>
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-
-            const SizedBox(height: 30.0,),
+            const SizedBox(
+              height: 30.0,
+            ),
 
             Image.asset(
               "images/fullLogo.png",
               width: 140,
             ),
 
-            const SizedBox(height: 16.0,),
+            const SizedBox(
+              height: 16.0,
+            ),
 
             //title
-             Text(
+            Text(
               "NEW TRIP REQUEST - ${widget.tripDetailsInfo!.busRoute}",
               style: TextStyle(
                 fontWeight: FontWeight.bold,
@@ -155,7 +164,9 @@ class _NotificationDialogState extends State<NotificationDialog>
               ),
             ),
 
-            const  SizedBox(height: 20.0,),
+            const SizedBox(
+              height: 20.0,
+            ),
 
             const Divider(
               height: 1,
@@ -163,27 +174,27 @@ class _NotificationDialogState extends State<NotificationDialog>
               thickness: 1,
             ),
 
-            const SizedBox(height: 10.0,),
+            const SizedBox(
+              height: 10.0,
+            ),
 
             //pick - dropoff
             Padding(
               padding: const EdgeInsets.all(16.0),
               child: Column(
                 children: [
-
                   //pickup
                   Row(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-
                       Image.asset(
                         "images/initial.png",
                         height: 16,
                         width: 16,
                       ),
-
-                      const SizedBox(width: 18,),
-
+                      const SizedBox(
+                        width: 18,
+                      ),
                       Expanded(
                         child: Text(
                           widget.tripDetailsInfo!.pickupAddress.toString(),
@@ -195,25 +206,25 @@ class _NotificationDialogState extends State<NotificationDialog>
                           ),
                         ),
                       ),
-
                     ],
                   ),
 
-                  const SizedBox(height: 15,),
+                  const SizedBox(
+                    height: 15,
+                  ),
 
                   //dropoff
                   Row(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-
                       Image.asset(
                         "images/final.png",
                         height: 16,
                         width: 16,
                       ),
-
-                      const SizedBox(width: 18,),
-
+                      const SizedBox(
+                        width: 18,
+                      ),
                       Expanded(
                         child: Text(
                           widget.tripDetailsInfo!.dropOffAddress.toString(),
@@ -225,15 +236,15 @@ class _NotificationDialogState extends State<NotificationDialog>
                           ),
                         ),
                       ),
-
                     ],
                   ),
-
                 ],
               ),
             ),
 
-            const SizedBox(height: 20,),
+            const SizedBox(
+              height: 20,
+            ),
 
             const Divider(
               height: 1,
@@ -241,7 +252,9 @@ class _NotificationDialogState extends State<NotificationDialog>
               thickness: 1,
             ),
 
-            const SizedBox(height: 8,),
+            const SizedBox(
+              height: 8,
+            ),
 
             //decline btn - accept btn
             Padding(
@@ -249,11 +262,9 @@ class _NotificationDialogState extends State<NotificationDialog>
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-
                   Expanded(
                     child: ElevatedButton(
-                      onPressed: ()
-                      {
+                      onPressed: () {
                         Navigator.pop(context);
                         // audioPlayer.stop();
                       },
@@ -268,13 +279,12 @@ class _NotificationDialogState extends State<NotificationDialog>
                       ),
                     ),
                   ),
-
-                  const SizedBox(width: 10,),
-
+                  const SizedBox(
+                    width: 10,
+                  ),
                   Expanded(
                     child: ElevatedButton(
-                      onPressed: ()
-                      {
+                      onPressed: () {
                         // audioPlayer.stop();
 
                         setState(() {
@@ -294,13 +304,13 @@ class _NotificationDialogState extends State<NotificationDialog>
                       ),
                     ),
                   ),
-
                 ],
               ),
             ),
 
-            const SizedBox(height: 10.0,),
-
+            const SizedBox(
+              height: 10.0,
+            ),
           ],
         ),
       ),
